ae34620ff9cb26949ba77202a2c1be833a3eed44
Ezequiel Cardoso <ezequielcardoso@gmail.com>
2021-09-06 11:36:31 -0300

    api fully functional and connected to react app

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get( '/', 'HomeController@index' ) ->name('home');
+Route::get('/', function () {
+  return view('app');
+});
+/* Route::get( '/', 'HomeController@index' ) ->name('home'); */
 
 /* Route::prefix('index')->group(function () {
     Route::get('/', 'HomeController@index');
 }); */
 
 Route::resource('about', 'AboutController');
-Route::prefix('about')->group(function() {
-    Route::post('search', "AboutController@search")->name("about.search");
+Route::prefix('about')->group(function () {
+  Route::post('search', "AboutController@search")->name("about.search");
 });
 
 Route::resource('participantes', 'ParticipantesController');
-Route::prefix('participantes')->group(function() {
-    Route::post('search', "ParticipantesController@search")->name("participantes.search");
+Route::prefix('participantes')->group(function () {
+  Route::post('search', "ParticipantesController@search")->name("participantes.search");
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
